9e90f27ee59e6affa2fb5f381c83498c338580e2
Kurt Jacobson <kcjengr@gmail.com>
2020-04-19 13:48:22 -0400

    clean up entry point names

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open("entry_points.ini", "r") as fh:
     entry_points = fh.read()
 
 setup(
-    name="Blue Control",
+    name="blue-control",
     version="0.0.1",
     author="Kurt Jacobson",
     author_email="<doe.john@example.com>",
     description="Blue Control - A QtPyVCP based Virtual Control Panel for LinuxCNC",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/USERNAME/REPO",
-    download_url="https://github.com/USERNAME/REPO/tarball/master",
+    url="https://github.com/kurtjacobson/blue-control",
+    download_url="https://github.com/kurtjacobson/blue-control/tarball/master",
     packages=find_packages(),
     include_package_data=True,
     entry_points=entry_points
